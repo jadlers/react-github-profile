47c0b7e93494a2c0ba2f9b4500fed2d651a8305c
Kent C. Dodds <kent@doddsfamily.us>
2018-11-08 23:03:48 -0700

    no more classes!!
    wow

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 import './global-styles.css'
-import React from 'react'
+import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom'
 import {Router} from '@reach/router'
+import ErrorBoundary from 'react-error-boundary'
 import ThemeProvider from './shared/theme-provider'
+import {IsolatedContainer} from './shared/pattern'
+import {Loading} from './shared/loading'
 import Home from './screens/home'
 import User from './screens/user'
 import * as GitHubContext from './github-client'
+
+function ErrorFallback({error}) {
+  return (
+    <IsolatedContainer>
+      <p>There was an error loading the data</p>
+      <pre style={{maxWidth: 700}}>{JSON.stringify(error, null, 2)}</pre>
+    </IsolatedContainer>
+  )
+}
 
 function App() {
   return (
     <ThemeProvider>
       <GitHubContext.Provider>
-        <Router>
-          <Home path="/" />
-          <User path="/:username" />
-        </Router>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense
+            maxDuration={2000}
+            fallback={
+              <IsolatedContainer>
+                <Loading />
+              </IsolatedContainer>
+            }
+          >
+            <Router>
+              <Home path="/" />
+              <User path="/:username" />
+            </Router>
+          </Suspense>
+        </ErrorBoundary>
       </GitHubContext.Provider>
     </ThemeProvider>
   )
@@ -22,5 +45,5 @@ function App() {
 
 const ui = <App />
 const container = document.getElementById('root')
-
-ReactDOM.render(ui, container)
+const root = ReactDOM.createRoot(container)
+root.render(ui)
